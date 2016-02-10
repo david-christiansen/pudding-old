506fee7f90cb6c66cf4793b62b1afa21d34cc8e8
David Raymond Christiansen <david@davidchristiansen.dk>
2016-02-10 16:20:20 -0500

    Update tactic-runner name

--- stlc-with-ui.rkt
+++ stlc-with-ui.rkt
@@ -11,16 +11,18 @@
 (module+ test
   (define test-term
     (proof #'(--> String Int)
-           (tactics (function-intro 'x)
-                    length-of-string
-                    (hypothesis 0))))
+           (begin-tactics
+             (function-intro 'x)
+             length-of-string
+             (hypothesis 0))))
   (define add-two
     (proof #'(--> Int Int)
-           (tactics (function-intro 'n)
-                    (addition 3)
-                    (int-intro 1)
-                    (hypothesis 0)
-                    (int-intro 1)))))
+           (begin-tactics
+             (function-intro 'n)
+             (addition 3)
+             (int-intro 1)
+             (hypothesis 0)
+             (int-intro 1)))))
 
 
 (module+ main
