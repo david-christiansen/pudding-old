fb31df94072278e1ff87c7e82f7b75b1fdeb8550
David Raymond Christiansen <david@davidchristiansen.dk>
2016-04-26 14:41:22 -0400

    Extract zippers to external package

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "refiners")
+(define deps '("base"
+               "rackunit-lib"
+               "zippers"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '())
+(define pkg-desc "Refinement logics in and for Racket")
+(define version "0.0.1")
