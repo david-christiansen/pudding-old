d584043310f493dbd46a00c53d3db2e756f0a5e2
David Raymond Christiansen <david@davidchristiansen.dk>
2016-05-16 08:53:14 -0400

    Coverage info

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"
                "rackunit-lib"
                "zippers"))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "cover-coveralls"))
 (define scribblings '())
 (define pkg-desc "Refinement logics in and for Racket")
 (define version "0.0.1")
