ee87c091f17dec4e1b95e6d4ed77e0690bbed0c8
David Raymond Christiansen <david@davidchristiansen.dk>
2016-06-09 15:28:12 -0400

    GUI improvements:
    Context, keybindings, and button activation.

--- presentation-gui.rkt
+++ presentation-gui.rkt
@@ -6,8 +6,113 @@
 (require syntax/parse)
 (require "error-handling.rkt" "infrastructure.rkt" "proof-state.rkt" "proofs.rkt" "metavariables.rkt")
 
+
+
 (define (hl pict)
   (frame pict))
+
+(define (hyp->pict h canvas)
+  (match h
+    [(hypothesis name assumption relevant?)
+     (define name-pict
+       (send canvas make-presentation name '(name)
+             (text (format "~v" (syntax-e name)))
+             hl))
+     (define assumption-pict
+       (term->pict assumption canvas))
+     (define (wrap p)
+       (if relevant?
+           (hb-append (text "[") p (text "]"))
+           p))
+     (wrap (hb-append name-pict (text " : ") assumption-pict))]))
+
+(define (sequent->pict seq canvas)
+  (match seq
+    [(>> H G)
+     (define H-pict
+       (if (null? H)
+           (cc-superimpose
+            (ghost (text "()"))
+            (filled-ellipse 3 3 #:color "black"))
+           (apply hb-append
+                  (add-between (map (lambda (h) (hyp->pict h canvas)) H)
+                               (text ", ")))))
+     (define G-pict
+       (term->pict G canvas))
+     (hb-append H-pict (text " >> ") G-pict)]))
+
+(define (sequent->big-pict seq canvas)
+  (match seq
+    [(>> H G)
+     (define context-pict
+       (apply vl-append
+              5
+              (for/list ([h H])
+                (hyp->pict h canvas))))
+     (define goal-pict
+       (term->pict G canvas))
+     (define width (max (pict-width context-pict) (pict-width goal-pict)))
+     (define line (filled-rectangle width 1 #:draw-border? #t))
+     (vl-append 10 context-pict line goal-pict )]))
+
+(define (on-box pict #:color [color "white"])
+  (cc-superimpose (filled-rectangle (pict-width pict) (pict-height pict) #:color color)
+                  pict))
+
+(define (opaque pict)
+  (cc-superimpose (filled-rectangle (pict-width pict) (pict-height pict)
+                                    #:color "white"
+                                    #:draw-border? #f)
+                  pict))
+
+(define (proof->pict proof canvas [indent-steps 0])
+  (define hspace 5)
+  (define vspace 10)
+  (define indent-space 20)
+  (define by (text "by" '(bold)))
+  (define left (text "<=" '(bold)))
+  (inset (send canvas make-presentation proof '(proof-step)
+               (match proof
+                 [(subgoal name goal)
+                  (define status (text "?" '(bold)))
+                  (define n (text (format "~v" name)))
+                  (define p (inset (hb-append
+                                    hspace
+                                    status
+                                    n
+                                    left
+                                    (sequent->pict goal canvas))
+                                   3))
+                  (on-box p)]
+                 [(refined-step name goal rule children extractor)
+                  (define status (text "➥" '(bold)))
+                  (define n (text (format "~v" name)))
+                  (define p
+                    (inset (hb-append hspace
+                                       status
+                                       n
+                                       left
+                                       (sequent->pict goal canvas)
+                                       (if rule
+                                           (hb-append hspace by (term->pict rule canvas))
+                                           empty))
+                           3))
+                  (apply vl-append
+                         vspace
+                         (on-box p)
+                         (for/list ([c children])
+                           (proof->pict c canvas (add1 indent-steps))))]
+                 [(complete-proof goal rule extract children)
+                  (define status (text "✔" '(bold)))
+                  (inset (hb-append hspace
+                                     status
+                                     (term->pict extract canvas)
+                                     left
+                                     (sequent->pict goal canvas))
+                         3)]
+                 [other (on-box (text (format "~v" other)))])
+               hl)
+         (* indent-space indent-steps) 0 0 0))
 
 (define (pprint-term stx canvas)
   (syntax-parse stx
@@ -17,10 +122,17 @@
       (lambda (str)
         (if (string? str)
             (send canvas make-presentation #'x '(name)
-                  (text str) hl)
+                  (opaque (text str)) hl)
             (send canvas make-presentation #'x '(name)
                   str hl)))
       (pp:text (symbol->string (syntax->datum #'x))))]
+    [x #:when (metavariable? (syntax-e #'x))
+       (pp:markup
+        (lambda (str)
+          (send canvas make-presentation (syntax-e #'x) '(metavariable)
+                (if (string? str) (opaque (text str)) str)
+                hl))
+        (pp:text (format "~v" (syntax-e #'x))))]
     [(lambda (xs:id ...) body)
      (pp:h-append
       pp:lparen
@@ -34,29 +146,25 @@
                             pp:rparen))
              (pprint-term #'body canvas)))
       pp:rparen)]
-    #;
-    [(? metavariable? (app syntax-e))
-     (pp:markup
-      (lambda (str)
-        (send canvas make-presentation #'x '())))]
     [(tm ...)
      (pp:h-append pp:lparen
                   (pp:v-concat/s (map (lambda (t) (pprint-term t canvas))
                                       (syntax-e #'(tm ...))))
                   pp:rparen)]
-    [other (pp:text (format "~v" (syntax->datum #'other)))]))
+    [other
+     (pp:text (format "~v" (syntax->datum #'other)))]))
 
 (define (term->pict stx canvas)
   (define (string->pict x)
     (if (string? x)
         (let ([lines (string-split x "\n" #:trim? #f)])
           (for/fold ([pict (blank)]) ([l lines])
-            (vl-append pict (text l))))
+            (vl-append pict (opaque (text l)))))
         x))
   (pp:pretty-markup
    (pprint-term stx canvas)
    (lambda (x y)
-     (hbl-append (string->pict x) (string->pict y)))
+     (hb-append (string->pict x) (string->pict y)))
    70))
 
 (define (extract-pict focus canvas)
@@ -84,11 +192,13 @@
     (super-new)
 
     (inherit change-style
-             get-text)
+             get-text
+             last-position
+             set-position)
 
     (send this set-styles-sticky #f)
 
-    ;;; Style for accepted region and editable region
+    ;; Style for accepted region and editable region
     (define accepted-bg
       (send (make-object style-delta%) set-delta-background
             "green"))
@@ -97,9 +207,9 @@
       (send (make-object style-delta%) set-delta-background
             "white"))
 
-    ;;; The editor positions that constitute the ends of each accepted
-    ;;; proof step. When advancing the accepted region, the new
-    ;;; position is consed on to this.
+    ;; The editor positions that constitute the ends of each accepted
+    ;; proof step. When advancing the accepted region, the new
+    ;; position is consed on to this.
     (define steps
       '(0))
     (define (save-step! pos)
@@ -115,92 +225,32 @@
     (define/augment (can-delete? start len)
       (>= start (car steps)))
 
-    ;;; Run a command
+    (define/public (can-advance?)
+      (for/or ([ch (in-string (get-text (car steps) 'eof #t))])
+        (not (char-whitespace? ch))))
+
+    (define/public (can-retract?)
+      (and (pair? steps)
+           (pair? (cdr steps))))
+
+    ;; Run a command
     (define/public (advance)
       (with-handlers ([exn? error-callback])
         (define step (car steps))
         (define text (get-text step))
         (define-values (val len) (read-with-length-from text))
-        (save-step! (+ step len))
-        (queue-callback (thunk (update-bg)))
-        (queue-callback (thunk (advance-callback val)))))
+        (queue-callback (thunk
+                         (with-handlers ([exn? error-callback])
+                           (advance-callback val)
+                           (set-position (+ step len))
+                           (save-step! (+ step len))
+                           (queue-callback (thunk (update-bg))))))))
 
     (define/public (retract)
       (when (and (pair? steps) (pair? (cdr steps)))
         (set! steps (cdr steps))
         (queue-callback (thunk (update-bg)))
         (queue-callback (thunk (retract-callback)))))))
-
-
-(define (vertically . imgs)
-  (define h 0)
-  (define todo
-    (for/list ([i imgs])
-      (define step
-        (list 0 h i))
-      (set! h (+ h (send i get-height)))
-      step))
-  (new compound-img% [imgs todo]))
-
-(define (horizontally . imgs)
-  (define w 0)
-  (define todo
-    (for/list ([i imgs])
-      (define step
-        (list w 0 i))
-      (set! w (+ w (send i get-width)))
-      step))
-  (new compound-img% [imgs todo]))
-
-(define (present-term tm)
-  (define tm-pict (inset (text (format "~v" (syntax->datum tm))) 8))
-  (new presentation-img%
-       [object tm]
-       [modality '(term)]
-       [img (new pict-img%
-                 [pict (cc-superimpose
-                        (filled-rectangle (pict-width tm-pict)
-                                          (pict-height tm-pict)
-                                          #:color "white"
-                                          #:draw-border #f)
-                        tm-pict)])]))
-
-(define (present-hypothesis h)
-  (match h
-    [(hypothesis name assumption relevant?)
-     (define name-p
-       (new presentation-img%
-            [object name]
-            [modality '(name term)]
-            [img (new pict-img% [pict (inset (text (format "~v" name)) 8)])]))
-     (define colon (text " : "))
-     (define assumption-p (present-term assumption))
-     ;; TODO relevant? handling
-     (new presentation-img%
-          [object h]
-          [modality '(hypothesis)]
-          [img (horizontally name-p colon assumption-p)])]
-    ))
-
-(define (present-goal g)
-  (match g
-    [(>> H G)
-     (let ([hyps-pres (reverse (map present-hypothesis H))]
-           [goal-pres (present-term G)])
-       (error 'todo))]))
-
-(define (present-step proof-step)
-  (match proof-step
-    [(dependent-subgoal waiting-for next)
-     (error 'nope)]
-    [(irrelevant-subgoal goal)
-     (error 'nope)]
-    [(subgoal name goal)
-     (error 'nope2)]
-    [(complete-proof goal rule extract children)
-     (error 'nope44)]
-    [(refined-step name goal rule children extractor)
-     (error 'ĺkjlkj)]))
 
 
 (define (prover-window namespace goal)
@@ -219,44 +269,98 @@
     (set! proof-state (car history))
     (set! history (cdr history)))
 
-  (define frame (new frame%
-                     [label "Pudding Prover"]
-                     [width 800]
-                     [height 600]))
-  (define stack (new vertical-panel% [parent frame]))
-  (define toolbar (new horizontal-panel% [parent stack] [stretchable-height #f]))
-  (define horiz (new panel:vertical-dragable% [parent stack]))
-  (define context (new panel:horizontal-dragable% [parent horiz]))
-  (define node-context (new presentation-canvas% [parent context]))
-  (define global-context (new tab-panel%
-                              [parent context]
-                              [choices '("Program" "Proof")]
-                              [callback (lambda (panel ev)
-                                          (when (eqv? (send ev get-event-type) 'tab-panel)
-                                            (match (send global-context get-selection)
-                                              [0 (send global-context change-children
-                                                       (thunk* (list program-view)))]
-                                              [1 (send global-context change-children
-                                                       (thunk* (list proof-view)))]
-                                              [other (error "Unknown tab")])))]))
-  (define program-view (new presentation-pict-canvas%
-                            [parent global-context]))
-  (define proof-view (new presentation-pict-canvas%
-                          [parent global-context]))
-  (define proof-script (new proof-script-text%
-                            [advance-callback
-                             (lambda (prog)
-                               (define st proof-state)
-                               (run-action (eval prog namespace))
-                               (checkpoint st))]
-                            [retract-callback undo]
-                            [error-callback displayln]))
-  (define proof-script-holder (new editor-canvas% [parent horiz] [editor proof-script]))
+  (define (on-error e)
+    (send error-view remove-all-picts)
+    (send error-view add-pict (text (exn-message e)) 5 5)
+    (send global-context set-selection 2)
+    (set-tab 2))
+
+  (define (set-tab i)
+    (match i
+      [0 (send global-context change-children
+               (thunk* (list program-view)))]
+      [1 (send global-context change-children
+               (thunk* (list proof-view)))]
+      [2 (send global-context change-children
+               (thunk* (list error-view)))]
+      [other (error "Unknown tab")]))
+
+  (define frame
+    (new frame%
+         [label "Pudding Prover"]
+         [width 800]
+         [height 600]))
+  (define stack
+    (new vertical-panel% [parent frame]))
+  (define toolbar
+    (new horizontal-panel% [parent stack] [stretchable-height #f]))
+  (define horiz
+    (new panel:vertical-dragable% [parent stack]))
+  (define context
+    (new panel:horizontal-dragable% [parent horiz]))
+  (define node-context
+    (new presentation-pict-canvas% [parent context]))
+  (define global-context
+    (new tab-panel%
+         [parent context]
+         [choices '("Program" "Proof" "Error")]
+         [callback (lambda (panel ev)
+                     (when (eqv? (send ev get-event-type) 'tab-panel)
+                       (set-tab (send global-context get-selection))))]))
+  (define program-view
+    (new presentation-pict-canvas%
+         [parent global-context]))
+  (define proof-view
+    (new presentation-pict-canvas%
+         [parent global-context]
+         [style '(deleted)]))
+  (define error-view
+    (new presentation-pict-canvas%
+         [parent global-context]
+         [style '(deleted)]))
+  (define proof-script
+    (new (class proof-script-text%
+           (super-new)
+           (define/override (on-char ev)
+             (update-buttons)
+             (super on-char ev)))
+         [advance-callback
+          (lambda (prog)
+            (send error-view remove-all-picts)
+            (define st proof-state)
+            (run-action (eval prog namespace))
+            (checkpoint st))]
+         [retract-callback undo]
+         [error-callback on-error]))
+
+  (define proof-script-keymap
+    (let ([map (send proof-script get-keymap)])
+      (send map add-function "advance" (thunk* (send proof-script advance)
+                                               (queue-callback update-views)))
+      (send map add-function "advance" (thunk* (send proof-script advance)
+                                               (queue-callback update-views)))
+      (send map map-function "m:n" "advance")
+      map))
+
+  (define proof-script-holder
+    (new editor-canvas% [parent horiz] [editor proof-script]))
 
   (define (update-views)
     (define focus (run-action get-focus))
     (send program-view remove-all-picts)
-    (send program-view add-pict (extract-pict focus program-view) 5 5))
+    (send program-view add-pict (extract-pict focus program-view) 5 5)
+    (send proof-view remove-all-picts)
+    (send proof-view add-pict (proof->pict focus proof-view) 5 5)
+    (send node-context remove-all-picts)
+    (with-handlers ([exn? displayln])
+      (send node-context add-pict
+            (sequent->big-pict (proof-step-goal focus) node-context)
+            5 5))
+    (update-buttons))
+
+  (define (update-buttons)
+    (send advance-button enable (send proof-script can-advance?))
+    (send retract-button enable (send proof-script can-retract?)))
 
   (define retract-button
     (new button%
